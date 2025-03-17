c01bb2dc7cc3b2cb61b99d7885e47d4e86f48ee5
karimmoustamid <moustamid.teck@gmail.com>
2025-03-17 15:07:37 -0600

    Add PlayerCharacter class and associated tests

---
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -0,0 +1,23 @@
+namespace GameEngine.Tests
+{
+    public class PlayerCharacterShould
+    {
+        [Fact]
+        public void BeInexperiencedWhenNew()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.True(sut.IsNoob);
+        }
+
+        [Fact]
+        public void CalculateFullName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+
+            Assert.Equal("Sarah Smith", sut.FullName);
+        }
+    }
+}
